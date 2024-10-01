5057647213d07d9b3e7cf40392521f21413213b4
CheckmateBasilisk <checkmatebasilisk@gmail.com>
2024-10-01 02:32:38 -0300

    added a few dependencies

---
+++ Api/core/core.go
@@ -0,0 +1,9 @@
+package core
+
+func SayFoo() string {
+    return "foo"
+}
+
+func SayBar() string {
+    return "bar"
+}
---
+++ Api/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+    "fmt"
+    "github.com/CheckmateBasilisk/library-api/core"
+    "github.com/CheckmateBasilisk/library-api/db"
+    "github.com/CheckmateBasilisk/library-api/server"
+)
+
+func main() {
+    fmt.Printf("starting database...\n")
+    fmt.Printf("starting server...\n")
+    fmt.Printf("done!...\n")
+
+    fmt.Println(db.Connect())
+
+    fmt.Printf(core.SayFoo())
+    fmt.Printf(core.SayBar())
+
+    server.Welcome()
+}
